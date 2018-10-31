12ad0ab1c31b8f2d9fe56df77d8a1907c71ed124
Maxwell Dylla <maxwelldylla2020@u.northwestern.edu>
2018-10-31 14:03:40 -0500

    cleanup static methods for experimental data

--- base/experimental_data.py
+++ base/experimental_data.py
@@ -6,13 +6,13 @@ from scipy.optimize import minimize
 import numpy as np
 
 '''
-this module abstracts experimental data as objects
+this module abstracts experimental data into structured objects
 '''
 
 
 class Sample(object):
     '''
-    handles experimental transport data for a single sample
+    abstraction of experimental transport data for a single sample
 
     Attributes:
         conductivity (ndarray) the experimental conductivity data. column one
@@ -44,14 +44,16 @@ class Sample(object):
     @staticmethod
     def extract_transport_function(seebeck, conductivity, temperature, s=1):
         '''
-        given an assumption of the transport mechanism (knowledge of s),
-        the transport function can be extracted from Seebeck-conductivity data
-        on a single sample. optimium is found by minimizing the absolute error
+        given an assumption of the transport mechanism (knowledge of s), the
+        transport function can be extracted from Seebeck-conductivity data on
+        a single sample. optimium is found by minimizing the absolute error
 
         Args:
-          seebeck: (float) the experimental Seebeck coefficient, V/K
-          conductivity: (float) experimental electrical conductivity, S/m
-          temperature: (float) the absolute temperature, K
+          seebeck (float) the experimental Seebeck coefficient, V/K
+          conductivity (float) experimental electrical conductivity, S/m
+          temperature (float) the absolute temperature, K
+          s (int|half-integer) assumption of transport exponent (mechanism)
+
 
         Returns: (float) the transport function prefactor sigma_E_0, S/m
         '''
@@ -62,66 +64,12 @@ class Sample(object):
             model_conductivity(cp, s, sigma_E_0) - conductivity),
             method='Nelder-Mead', x0=[0.]).x[0]
 
-
-class SampleSeries(object):
-    '''
-    a sample series contains multiple samples of the same material but
-    different doping levels (assumed that bands do not change with doping)
-
-    Attributes:
-        samples ([Sample]) list of Samples, which have cond and seeb attributes
-    '''
-    def __init__(self, samples=None):
-        if not samples:
-            self.samples = []
-        else:
-            self.samples = samples
-
-    def from_path(self, path):
-        '''
-        load a series of sample data in path. files should be named according
-        to the specifications in Sample.from_csv()
-
-        Args:
-            path (str) path to data files, both conductivity and seebeck data
-                files should be in the same path
-        '''
-        names = {file.split('_')[0] for file in listdir(path)
-                 if '.csv' in file}
-        for name in names:
-            self.samples.append(Sample.from_csv(name, path))
-
-    @staticmethod
-    def jonker_analysis(seebecks, conductivities, temperature, s=1):
-        '''
-        a jonker analysis is typicaly applied to a series of samples
-        with the same electronic structure but different doping concentrations.
-        properties of each sample should be measured at the same temperature
-
-        Args:
-          seebecks: ([float]) the Seebeck coefficients, V/K
-          conductivities: ([float]) electrical conductivities, S/m
-          temperature: (float) the absolute temperature, K
-          s: (int|half-integer) assumption of transport exponent (mechanism)
-
-        Returns: (touple of floats) transport prefactors sigma_E_0, S/m
-          - average value from all samples
-          - minimum value from one sample
-          - maximum value from one sample
-        '''
-
-        trans_funcs = []
-        for seeb, cond in zip(seebecks, conductivities):
-            trans_funcs.append(
-                Sample.extract_transport_function(seeb, cond, temperature, s))
-        return (np.mean(trans_funcs), min(trans_funcs), max(trans_funcs))
-
     @staticmethod
     def temperature_analysis(seebecks, conductivities, temperatures, s=1):
         '''
-        temperature analysis is carried out for a single sample
-        the temperature dependence of the transport function is informative
-        when checking for multi-band behavior or strange scattering mechanisms
+        temperature analysis is carried out for a single sample by extracting
+        the transport function verses temperature. it is informative when
+        checking for multi-band behavior or strange scattering mechanisms
 
         Args:
           seebecks: ([float]) the Seebeck coefficients, V/K
@@ -137,3 +85,57 @@ class SampleSeries(object):
             trans_funcs.append(
                 Sample.extract_transport_function(seeb, cond, temp, s))
         return trans_funcs
+
+
+class SampleSeries(object):
+    '''
+    a sample series contains multiple samples. a sample series will typically
+    be the same base material with different doping concentrations
+
+    Attributes:
+        samples ([Sample]) list of Samples, which have cond and seeb attributes
+    '''
+    def __init__(self, samples=None):
+        if not samples:
+            self.samples = []
+        else:
+            self.samples = samples
+
+    def from_path(self, path):
+        '''
+        load a series of sample data in path. the data files must be named
+        according to the following scheme -- name_cond.csv/name_seeb.csv
+
+        Args:
+            path (str) path to data files, both conductivity and seebeck data
+                files should be in the same path
+        '''
+        names = {file.split('_')[0] for file in listdir(path)
+                 if '.csv' in file}
+        for name in names:
+            self.samples.append(Sample.from_csv(name, path))
+
+    @staticmethod
+    def jonker_analysis(seebecks, conductivities, temperature, s=1):
+        '''
+        a jonker analysis is typicaly applied to a series of samples with the
+        same electronic structure but different doping concentrations.
+        properties of each sample should be measured at the same temperature
+
+        Args:
+          seebecks ([float]) the Seebeck coefficients, V/K
+          conductivities ([float]) electrical conductivities, S/m
+          temperature (float) the absolute temperature, K
+          s (int|half-integer) assumption of transport exponent (mechanism)
+
+        Returns: (touple of floats) transport prefactors sigma_E_0, S/m
+          - average value from all samples
+          - minimum value from the samples
+          - maximum value from the samples
+        '''
+
+        trans_funcs = []
+        for seeb, cond in zip(seebecks, conductivities):
+            trans_funcs.append(
+                Sample.extract_transport_function(seeb, cond, temperature, s))
+        return (np.mean(trans_funcs), min(trans_funcs), max(trans_funcs))
